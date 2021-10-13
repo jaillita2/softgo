9ebc8e287bef6f3d680a223e67ed871047154601
Limberg Villca Coraite <201907693@est.umss.edu>
2021-10-13 16:35:09 -0400

    code user
    code create user

--- resources/views/registrer/createUser.blade.php
+++ resources/views/registrer/createUser.blade.php
@@ -3,6 +3,28 @@
 @section('title', 'Create User')
 
 @section('content')
-    <h1>Proximamente...Pagina para crear usuarios</h1>
+
+    <label for="Nombre_usuario"> Nombre de usuario </label>
+    <input type="text" name="Nombre_usuario" id="Nombre_usuario">
+    <br>
+    <label for="Contraseña"> Contraseña </label>
+    <input type="text" class="Contraseña" id="Contraseña">
+    <br>
+    <label for="Confirmar_contraseña"> Confirmar contraseña </label>
+    <input type="text" class="Confirmar_contraseña" id="Confirmar_contraseña">
+    <br>
+    <label for="Direccion_correo"> Direccion de correo </label>
+    <input type="text" class="Direccion_correo" id="Direccion_correo">
+    <br>
+    <label for="Apellidos"> Apellido(s) </label>
+    <input type="text" class="Apellidos" id="Apellidos">
+    <br>
+    <label for="Nombre"> Nombre </label>
+    <input type="text" class="Nombre" id="Nombre">
+    <br>
+    <input type="submit" value="Crear usuario" id="Crear">
+    <br>
+    <input type="submit" value="Cancelar" id="Cancelar">
+    <br>
 @endsection
 
