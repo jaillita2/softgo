ed1f4929e522073f26c2b3b1a5b7adbac27c89f3
LimbergVCoraite <91810939+LimbergVCoraite@users.noreply.github.com>
2021-11-23 15:38:54 -0500

    solucion
    bug de specs

--- app/Http/Controllers/EspecificacionesController.php
+++ app/Http/Controllers/EspecificacionesController.php
@@ -41,7 +41,7 @@ class EspecificacionesController extends Controller
         $datosEsp = request()->except('_token');
         
         if($request->hasfile('DOC_ESP')){
-            $datosConv['DOC_ESP']=$request->file('DOC_ESP')->store('uploads','public');
+            $datosEsp['DOC_ESP']=$request->file('DOC_ESP')->store('uploads','public');
         }
 
         especificaciones::insert($datosEsp);
