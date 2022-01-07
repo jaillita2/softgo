434e71ffef14bc53e379e0a429f43972d69944a1
LimbergVCoraite <91810939+LimbergVCoraite@users.noreply.github.com>
2022-01-07 11:24:15 -0400

    ACTUAL

--- app/Http/Requests/StoreGrupo.php
+++ app/Http/Requests/StoreGrupo.php
@@ -26,10 +26,10 @@ class StoreGrupo extends FormRequest
         return [
             'NAME_EMP'=> 'required|max:25|unique:grupo_empresas',
             'EMAIL_DOCE'=> 'required|max:30|unique:grupo_empresas|email',
-            'DIRECCION'=> 'required|max:30',
-            'TELEFONO'=> 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10|max:18',
+            'DIRECCION'=> 'required|max:50',
+            'TELEFONO'=> 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:8|max:18',
             'SOCIOS'=> 'required|max:400',
-            'ENC_GR'=> 'required|max:35|unique:grupo_empresas|exists:user__creates,NAME'
+            'ENC_GR'=> 'required|max:35|unique:grupo_empresas'
         ];
     }
 }
